e6c9900a6cfcd5199ffadd160a165a5c1c35f18d
Martin Majlis <martin@majlis.cz>
2023-02-15 06:19:30 +0100

    Add initial version of the script
    Run:
      * ./logger.sh -f text -o stderr -d 3
      * ./logger.sh -f json -o stdout -d 5
      * ./logger.sh -f text -o /var/log/logger.log -d 7

---
+++ logger.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+output=stdout
+delay=5
+format=text
+
+help()
+{
+    echo "Usage: logger [ -o | --output ]
+              [ -d | --delay ]
+              [ -f | --format ]
+              [ -h | --help  ]
+
+         output:
+             where will be the log produced
+             stdout, stderr, or path to the output file
+             default: ${output}
+         delay:
+             delay between log lines in seconds
+             default: ${delay}
+        format:
+            format of the output line
+            text or json
+            default: ${format}"
+    exit 2
+}
+
+SHORT=o:,d:,f:,h
+LONG=output:,delay:,format:,help
+OPTS=$(getopt -a -n logger --options $SHORT --longoptions $LONG -- "$@")
+
+eval set -- "$OPTS"
+
+while :
+do
+    case "$1" in
+        -o | --output )
+            output="$2"
+            shift 2
+            ;;
+        -d | --delay )
+            delay="$2"
+            shift 2
+            ;;
+        -f | --format )
+            format="$2"
+            shift 2
+            ;;
+        -h | --help)
+            help
+            ;;
+        --)
+            shift;
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1"
+            help
+            ;;
+    esac
+done
+
+echo "Output: ${output}; Delay: ${delay}; Format: ${format}"
+
+while :
+do
+    case "${format}" in
+        txt | text )
+            line="`date  --iso-8601=seconds` output=${output}; delay=${delay}; format=${format}"
+            ;;
+        json )
+            line="{\"date\": \"`date  --iso-8601=seconds`\", \"output\": \"${output}\", \"delay\": \"${delay}\", \"format\": \"${format}\", \"nested\": {\"a\": 1, \"b\": 2, \"c\": 3}}"
+            ;;
+    esac
+
+    case "${output}" in
+        stdout )
+            echo "${line}"
+            ;;
+        stderr )
+            echo "${line}" >&2
+            ;;
+        *)
+            echo "${line}" > "${output}"
+            ;;
+    esac
+
+
+    sleep ${delay};
+done
